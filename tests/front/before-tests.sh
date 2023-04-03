3acfc30ee29b2fb04c87e2e4c58772a500882053
Gabriel Martins <gabtec@sapo.pt>
2023-04-03 17:34:54 +0100

    fix: test call

--- tests/front/before-tests.sh
+++ tests/front/before-tests.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo "Preparing env before testing..."
-
-echo "Cleaning old tests..."
-./after-test.sh 
-
 echo "Generate js file from template..."
 
 cp $PWD/website/scripts.tpl $PWD/website/scripts2.js
